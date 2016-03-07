0f2be7c2be4d72c7b1e25e280102ad38770084b6
sophiallen <smlelevich@gmail.com>
2016-03-07 10:48:53 -0800

    added tooltip plugin

--- js/services.js
+++ js/services.js
@@ -1,4 +1,5 @@
 $(function() {
-$( "#tabs" ).tabs();
-$("#datepicker").datepicker();
+$(document).tooltip(); // tooltip  (on contact page's phone number element).
+$( "#tabs" ).tabs(); //tabs (used on services page). 
+$("#datepicker").datepicker(); //date picker (used on contact page). 
 });
